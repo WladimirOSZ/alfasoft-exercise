9cd1ecdb2aa5493c623c9386eeb80b6437852501
Wladimir Oliveira Souza <wladimir5002@gmail.com>
2023-05-27 16:20:23 -0300

    Refactoring the name of request

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,19 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contact;
-use App\Http\Requests\StoreContactRequest;
+use App\Http\Requests\ContactRequest;
 use App\Http\Services\ContactService;
 
 
 class ContactController extends Controller
 {
-
-    // public function __construct()
-    // {
-    //     $this->middleware('auth')->except('index');
-    // }
-
-
     public function index(){
         $contacts = Contact::all();
         return view('dashboard', compact('contacts'));
@@ -30,7 +23,7 @@ class ContactController extends Controller
         return view('contacts.create');
     }
 
-    public function store(StoreContactRequest $request){
+    public function store(ContactRequest $request){
         ContactService::store($request);
         return redirect()->route('contacts.index')->with('message', 'Contact created successfully.');
     }
@@ -39,7 +32,7 @@ class ContactController extends Controller
         return view('contacts.edit', compact('contact'));
     }
 
-    public function update(Contact $contact, StoreContactRequest $request){
+    public function update(Contact $contact, ContactRequest $request){
         ContactService::update($contact, $request);
         return redirect()->route('contacts.index')->with('message', 'Contact updated successfully.');
     }
--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class StoreContactRequest extends FormRequest
+class ContactRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
